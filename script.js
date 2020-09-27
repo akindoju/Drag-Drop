4a1f3de705eaa79e2580ef5d0dc89a1c9bb662d1
Akndju <davidd.akindoju@gmail.com>
2020-09-27 23:04:22 +0100

    Added Drag Drop functionality

--- script.js
+++ script.js
@@ -48,11 +48,20 @@ function createList() {
   addEventListeners();
 }
 
-function dragStart() {}
+function dragStart() {
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
 
-function dragOver() {}
+function dragOver(e) {
+  e.preventDefault();
+}
 
-function dragDrop() {}
+function dragDrop() {
+  const dragEndIndex = +this.closest('li').getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
 
 function dragEnter() {
   this.classList.add('over');
@@ -60,6 +69,27 @@ function dragEnter() {
 
 function dragLeave() {
   this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector('.draggable').innerText.trim();
+
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
 }
 
 function addEventListeners() {
@@ -77,3 +107,5 @@ function addEventListeners() {
     items.addEventListener('dragleave', dragLeave);
   });
 }
+
+check.addEventListener('click', checkOrder);
